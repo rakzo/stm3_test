0cd4c8e56f35285b516915da543aada1934c4db9
rakzo <oscarvelandia02@gmail.com>
2014-11-18 08:22:10 -0500

    commit inicial de stm3_test

---
+++ stm3_test/src/main/main.java
@@ -0,0 +1,15 @@
+package main;
+
+public class main {
+
+	public main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Nuevo Proyecto");
+
+	}
+
+}
