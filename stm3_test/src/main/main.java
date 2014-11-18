a091e05e229ff59c70cf6234aef5d62552209a40
rakzo <oscarvelandia02@gmail.com>
2014-11-18 08:26:37 -0500

    primer cambio rama master

--- stm3_test/src/main/main.java
+++ stm3_test/src/main/main.java
@@ -9,6 +9,8 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Nuevo Proyecto");
+		System.out.println("1er cambioen rama master");
+		
 
 	}
 
